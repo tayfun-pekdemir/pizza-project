8253e2abf936087ddbfabfc99670d5d653210cc8
Tayfun <tayfun.software96@gmail.com>
2025-09-06 22:38:48 +0300

    SuccessPage bileşenleri eklendi ve CSS'leri yazıldı.

--- src/pages/SuccessPage.jsx
+++ src/pages/SuccessPage.jsx
@@ -1,6 +1,8 @@
 export default function SuccessPage () {
     return(
-        <>
-        </>
+        <div className="success-page">
+            <img src="../images/iteration-1-images/logo.svg" alt="Logo" />
+            <h1>TEBRİKLER!<br/>SİPARİŞİNİZ ALINDI!</h1>
+        </div>
     )
 }
