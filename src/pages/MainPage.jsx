e276f2c5acb40d3728611d157828cbb0131c2fc7
Tayfun <tayfun.software96@gmail.com>
2025-09-14 19:09:50 +0300

    Navbar componenti oluşturulup MainPage'e eklendi ve CSS yazıldı.

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,19 +1,24 @@
 import { useHistory } from "react-router-dom";
 import { Button } from "reactstrap";
 import Footer from "../components/Footer.jsx";
+import Navbar from "../components/Navbar.jsx";
 
-export default function MainPage () {
+export default function MainPage() {
 
     const history = useHistory();
 
-    return(
-        <>
+    return (
+        <div className="main-page">
             <div className="hero-banner">
                 <img src="../images/iteration-1-images/logo.svg" alt="Logo" />
-                <h1>KOD ACIKTIRIR<br/>PİZZA, DOYURUR</h1>
-                <Button type="button" onClick={()=>{history.push("/order")}}>ACIKTIM</Button>
+                <h1>KOD ACIKTIRIR<br />PİZZA, DOYURUR</h1>
+                <Button type="button" onClick={() => { history.push("/order") }}>ACIKTIM</Button>
             </div>
-            <Footer/>
-        </>
+            <Navbar />
+            <main>
+
+            </main>
+            <Footer />
+        </div>
     )
 }
